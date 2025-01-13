4394858c4e53e111fecb4c94f1df2da51e5df9f6
Andres L <darkobito@gmail.com>
2025-01-13 20:38:19 +1300

    Uploading FIles as Errors

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
+let x = 100;
+let y = 0;
+let z = 1;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 500);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  
+  stroke(255, 255, 255)
+  fill(255, 255, 0)
+  circle(100, y, z)
+  
+  y = y + 6;
+  z = z + 4;
+  
+  if (y > 500){
+    y = height
+    
+  }
+  
+  else {y = y + 15;
+  z = z - 2;  
+       }
+  
+  {stroke(255, 255, 255)
+  fill(0, 0, 0)
+  triangle(20, 500, 180, 500, 100, 420);}
 }
+
